aa3dadbb30ebd8b188474a225d4538f032808b54
Dmitrij Kolotusha <suvstreet@gmail.com>
2024-01-31 19:11:49 +0300

    project without JSX

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,28 +1,41 @@
 import logo from './logo.svg'
 import './App.css'
+import React from 'react'
 
 export const App = () => {
-	// императивный стиль, описываем какая структура HTML должна вернуться
-	return (
-		// декларативный стиль, мы просто описываем,
-		// что хотим получить на каждой строчке,
-		// не задумываясь о пикселях, о свойстве className и т.д.
-		<div className='App'>
-			<header className='App-header'>
-				<img src={logo} className='App-logo' alt='logo' />
-				<p>
-					Edit <code>src/App.js</code> and save to reload 2.
-				</p>
-				<a
-					className='App-link'
-					href='https://reactjs.org'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					Learn React
-				</a>
-				<p>{new Date().getFullYear()}</p>
-			</header>
-		</div>
+	// императивный стиль, описываем какая структура HTML должна вернуться.
+	// А далее декларативный стиль, мы просто описываем,
+	// что хотим получить на каждой строчке с помощью React.createElement,
+	// не задумываясь как он будет это создовать элемент, распологать
+	return React.createElement(
+		'div',
+		{ className: 'App' },
+		React.createElement(
+			'header',
+			{ className: 'App-header' },
+			React.createElement('img', {
+				src: logo,
+				className: 'App-logo',
+				alt: 'logo',
+			}),
+			React.createElement(
+				'p',
+				null,
+				'Edit ',
+				React.createElement('code', null, 'src/App.js'),
+				' and save to reload 2.',
+			),
+			React.createElement(
+				'a',
+				{
+					className: 'App-link',
+					href: 'https://reactjs.org',
+					target: '_blank',
+					rel: 'noopener noreferrer',
+				},
+				'Learn React',
+			),
+			React.createElement('p', null, new Date().getFullYear()),
+		),
 	)
 }
